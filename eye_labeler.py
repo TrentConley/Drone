5ff27f31e9af4d63532f6da7f7d260bd8733a4dc
Trent Conley <trentconley@gmail.com>
2023-09-15 17:06:54 -0400

    finding different algo for eye detection

--- eye_labeler.py
+++ eye_labeler.py
@@ -5,11 +5,10 @@ import json
 # Initialize webcam
 cap = cv2.VideoCapture(0)
 
-# Load the pre-trained Haar Cascade classifiers for face and eyes
+# Load the pre-trained Haar Cascade classifiers for face
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
 
 # Create a directory to save the images and labels
 os.makedirs("dataset", exist_ok=True)
@@ -27,7 +26,7 @@ while True:
     # Create a copy of the frame before drawing rectangles
     original_frame = frame.copy()
 
-    # Convert to grayscale
+    # Convert to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces
@@ -38,68 +37,53 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         # Get the region of interest to find eyes
-        roi_gray = gray[y : y + h, x : x + w]
         roi_color = frame[y : y + h, x : x + w]
 
-        # Detect eyes
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        # Convert the ROI to HSV color space
+        hsv_roi = cv2.cvtColor(roi_color, cv2.COLOR_BGR2HSV)
+
+        # Create a mask for white color (common for sclera)
+        lower_white = (0, 0, 200)
+        upper_white = (180, 255, 255)
+        mask = cv2.inRange(hsv_roi, lower_white, upper_white)
+
+        # Find contours in the mask
+        contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
         eye_labels = []
-        for ex, ey, ew, eh in eyes:
-            # Adjust eye coordinates
-            ex += x
-            ey += y
+        for cnt in contours:
+            ex, ey, ew, eh = cv2.boundingRect(cnt)
+
+            # Ignore small contours that are not likely to be eyes
+            if ew * eh < 100:
+                continue
+
+            # Adjust eye coordinates relative to the whole frame
+            ex, ey = ex + x, ey + y
+
             # Draw rectangle around the eyes
             cv2.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-            eye_labels.append((int(ex), int(ey), int(ew), int(eh)))
+            eye_labels.append((ex, ey, ew, eh))
 
         # Associate eyes with the face
         face_eye_dict[str((x, y, w, h))] = eye_labels
 
-    while True:
-        # Display the frame with face and eye rectangles
-        cv2.imshow("Face and Eye Detection", frame)
+    # Display the frame with face and eye rectangles
+    cv2.imshow("Face and Eye Detection", frame)
 
-        # Wait for user to press 'Enter' to save the frame and labels, or 'd' to delete the last rectangle
-        key = cv2.waitKey(0)
-        if key == 13:  # Enter key
-            cv2.imwrite(f"dataset/frame_{frame_count}.png", original_frame)
-            with open(f"dataset/labels_{frame_count}.json", "w") as f:
-                json.dump(face_eye_dict, f)
-            frame_count += 1
-            break
-        elif key == ord("d"):
-            if face_eye_dict:
-                last_face = list(face_eye_dict.keys())[-1]
-                if face_eye_dict[last_face]:
-                    face_eye_dict[last_face].pop()
-                else:
-                    del face_eye_dict[last_face]
-                frame = original_frame.copy()  # Create a new copy of the original frame
-                for face, eyes in face_eye_dict.items():
-                    face = tuple(map(int, face.strip("()").split(",")))
-                    cv2.rectangle(
-                        frame,
-                        (face[0], face[1]),
-                        (face[0] + face[2], face[1] + face[3]),
-                        (255, 0, 0),
-                        2,
-                    )
-                    for eye in eyes:
-                        cv2.rectangle(
-                            frame,
-                            (eye[0], eye[1]),
-                            (eye[0] + eye[2], eye[1] + eye[3]),
-                            (0, 255, 0),
-                            2,
-                        )
+    # Wait for user input
+    key = cv2.waitKey(1)
 
-        # Press 'q' to quit the application
-        if key == ord("q"):
-            break
-
-    # If 'q' was pressed, break the outer loop as well
+    # Press 'q' to quit the application
     if key == ord("q"):
         break
+
+    # Press 's' to save the current frame and labels
+    elif key == ord("s"):
+        cv2.imwrite(f"dataset/frame_{frame_count}.png", original_frame)
+        with open(f"dataset/labels_{frame_count}.json", "w") as f:
+            json.dump(face_eye_dict, f)
+        frame_count += 1
 
 # Release the capture and destroy all OpenCV windows
 cap.release()
